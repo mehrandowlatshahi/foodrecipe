e4393094cddcf8f6a18701c9f9e1f2655bdef9c2
mehran.dowlatshahi <mehdow@gmail.com>
2015-04-28 07:36:33 +1000

    added recipes and its controller for input of recipes

---
+++ src/main/java/mehpac/Recipes.java
@@ -0,0 +1,14 @@
+package mehpac;
+
+public class Recipes {
+	private String jsonRecipes;
+
+	public String getJsonRecipes() {
+		return jsonRecipes;
+	}
+
+	public void setJsonRecipes(String jsonRecipes) {
+		this.jsonRecipes = jsonRecipes;
+	}
+	
+}
